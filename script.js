3faf55c77c99e1de25b0d1812f35c3b01edbbace
ridalo <ridalo1989@gmail.com>
2021-07-18 14:48:50 -0400

    finished javascript.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let countEl =  document.getElementById("count-el")
 console.log(countEl)
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
-    
-    
+
 }
 
+function Save() {
+let countSave = count + " - "
+saveEl.textContent += countSave
+countEl.textContent = 0
+count = 0
+}
+
+Save()
